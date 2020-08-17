1c46843d03655e4008be8cdd80b4bb2cc48b0632
Nico <nicolasgoncalvesng1@gmail.com>
2020-08-17 02:14:47 +0200

    Added a timer to 50k counter

--- 50k-counter.py
+++ 50k-counter.py
@@ -1,7 +1,12 @@
 import os
+import timeit
 os.system("color 2")
+timerstart = timeit.default_timer()
 a = 0
 while a <= 50000:
     a += 1
     print(a)
-    
+timerstop = timeit.default_timer()
+timerresult = timerstop - timerstart
+print("time result: ", "%.2f" % timerresult, "seconds")
+input()
